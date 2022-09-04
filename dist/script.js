947fb20abf765d5f0ba8c38f3a15eba879868778
Simplyvoda <efem.vodina@gmail.com>
2022-09-04 18:02:48 +0100

    commit

--- dist/script.js
+++ dist/script.js
@@ -2,12 +2,18 @@ const inputText = document.getElementById('email-text');
 
 function ValidateEmail(inputText) {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (inputText.value.match(mailformat)) {
-        document.getElementById("error-message").style.display = "none";
-        return true;
+    if (inputText !== null) {
+        if (inputText.value.match(mailformat)) {
+            document.getElementById("error-message").style.display = "none";
+            return true;
+        }
+        else {
+            document.getElementById("error-message").style.display = "block";
+            return false;
+        }
     }
     else {
-        document.getElementById("error-message").style.display = "block";
-        return false;
+        document.getElementById("null-message").style.display = "block"; //this line doesn't work
     }
+
 }
